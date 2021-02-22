e913b8aca4223cb1a3cd207f4a929dd2f51931f2
Ryota Horiguchi <ryotanoaddress@gmail.com>
2021-02-22 18:51:16 +0900

    Load data

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 createApp(App)
   .use(router)
+  .use(VueAxios, axios)
   .mount("#app");
--- src/views/Foo.vue
+++ src/views/Foo.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
     <h1>Foo</h1>
+    {{ todo }}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      todo: undefined,
+    };
+  },
+  created() {
+    const path = "https://jsonplaceholder.typicode.com/todos/1";
+
+    fetch(path)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    // => {userId: 1, id: 1, title: "delectus aut autem", completed: false}
+
+    this.axios.get(path).then((response) => {
+      console.log(response.data);
+      // => {userId: 1, id: 1, title: "delectus aut autem", completed: false}
+
+      this.todo = response.data;
+    });
+  },
+};
+</script>
